9ea51102bb02525cd53c134d9f9595c5c6c4baf7
Derek Warren <warren@sfu.ca>
2013-07-24 12:39:37 -0700

    Debian/Ubuntu support.
    Verified working against SSSD/LDAP/AD. New sss auth-client-config
    profile for Debian/Ubuntu.

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,12 +1,13 @@
 class sssd::config (
   $domains,
-  $local_groups,
-  $use_ecryptfs,
-) {
+  $services,
+  ) {
 
   concat { '/etc/sssd/sssd.conf':
     warn    => true,
     mode    => '0600',
+    owner	=> 'root',
+    group	=> 'root',
   }
 
   concat::fragment { 'sssd.conf.base':
@@ -14,46 +15,43 @@ class sssd::config (
     order   => 02,
     content => template('sssd/sssd.conf.erb'),
   }
+
+  case $::osfamily {
+    debian: {
+
+      # SSSD automount retrieval in autofs 5.0.7 is busted in Ubuntu
+      # see https://bugs.launchpad.net/linuxmint/+bug/1081489 for a fix
+      file { '/etc/auth-client-config/profile.d/sss':
+	ensure  => file,
+	content => template('sssd/sss.erb'),
+	notify  => Exec['update-sssd-profile'],
+      }
+      exec { 'update-sssd-profile':
+	command     => '/usr/sbin/auth-client-config -a -p sss',
+	require     => File[ '/etc/auth-client-config/profile.d/sss' ],
+	refreshonly => true,
+      }
+    }
+
+    redhat: {
+      include autofs
+      if member($services, 'autofs') {
+        augeas { 'nsswitch.conf':
+          context => '/files/etc/nsswitch.conf',
+          changes => ["set /files/etc/nsswitch.conf/*[self::database = 'automount']/service[1] sss",
+                      "set /files/etc/nsswitch.conf/*[self::database = 'automount']/service[2] files",],
+          notify => [ Service[sssd], Service[autofs], ],
+        }
+      }
+      else {
+        augeas { 'nsswitch.conf':
+          context => '/files/etc/nsswitch.conf',
+          changes => ["set /files/etc/nsswitch.conf/*[self::database = 'automount']/service[1] files",
+                      "rm /files/etc/nsswitch.conf/*[self::database = 'automount']/service[2]",],
+          notify => [ Service[sssd], Service[autofs], ],
+        }
+      }
+    }
+  }
+  }
   
-  file { '/etc/security/group.conf':
-    ensure  => file,
-    content => template('sssd/group.conf.erb'),
-  }
-
-  file { '/etc/auth-client-config/profile.d/sss':
-    ensure  => file,
-    content => template('sssd/sss.erb'),
-    notify  => Exec['update-sssd-profile'],
-  }
-
-  exec { 'update-sssd-profile':
-    command     => 'auth-client-config -a -p sss',
-    require     => File[ '/etc/auth-client-config/profile.d/sss' ],
-    refreshonly => true,
-  }
-
-  if $use_ecryptfs {
-    package { 'libpam-script':
-      ensure => present
-    }
-
-    file { '/usr/share/libpam-script/pam_script_ses_open':
-      ensure  => file,
-      source  => 'puppet:///modules/sssd/pam_script_ses_open',
-      mode    => '0755',
-      require => Package['libpam-script']
-    }
-
-    file { '/usr/local/bin/ecryptfs-wrap-passphrase-1st-login.sh':
-      ensure  => file,
-      source  => 'puppet:///modules/sssd/ecryptfs-wrap-passphrase-1st-login.sh',
-      mode    => '0755',
-    }
-
-    file { '/usr/share/autostart/ecryptfs-wrap-passphrase-1st-login.desktop':
-      ensure  => file,
-      source  => 'puppet:///modules/sssd/ecryptfs-wrap-passphrase-1st-login.desktop',
-      mode    => '0644',
-    }
-  }
-}
--- manifests/domain.pp
+++ manifests/domain.pp
@@ -1,28 +1,30 @@
 define sssd::domain (
-  $ldap_uri,
-  $ldap_search_base,
-  $ldap_group_search_base,
-  $ldap_schema            = 'rfc2307',
-  $ldap_id_use_start_tls  = true,
-  $tls_ca                 = false,
-  $tls_chain              = false,
+  $debug_level		    = '5',
+  $krb_use_fast		    = 'try',
+
+  # Samba insists on using uppercase for the hostname when joining AD.
+  # Convert to uppercase or SSSD will complain principal machine$@AD.SFU.CA
+  # can't be found in keytab. Thanks, Riley!
+  $ldap_sasl_authid = inline_template("<%= hostname.upcase %>\$@AD.SFU.CA"),
+
+
+  # don't use units EVER; assume seconds
+  # units will confuse authconfig on CentOS 6
+  $krb5_renewable_lifetime    = '86400',
+  $krb5_renew_interval 	      = '1800',
+  $entry_cache_autofs_timeout = '600',
+  $autofs_provider            = 'ldap',
+  $ldap_autofs_search_base    = '',
+  $ldap_autofs_map_object_class   = 'nisMap',
+  $ldap_autofs_entry_object_class = 'nisObject',
+  $ldap_autofs_map_name       = 'cn',
+  $ldap_autofs_entry_key      = 'nisMapName',
+  $ldap_autofs_entry_value    = 'nisMapEntry',  
 ) {
-  include ssl::variables
 
-  if ! ( $tls_ca ) and ! ( $tls_chain ) {
-    fail ( 'You must provide either $tls_ca or $tls_chain' )
-  }
+#notify{"SASL binding to AD as $ldap_sasl_authid":;}
 
-  if $tls_ca {
-    $tlscacertificatefile = "${ssl::variables::ssl_certs}/cert_${tls_ca}.pem"
-    Ssl::Cert[$tls_ca] -> Domain[ $name ]
-  }
-  elsif $tls_chain {
-    $tlscacertificatefile = "${ssl::variables::ssl_chain}/chain_${tls_chain}.pem"
-    Ssl::Chain[$tls_chain] -> Domain[ $name ]
-  }
-
-  concat::fragment { "sssd_domain_${name}":
+concat::fragment { "sssd_domain_${name}":
     target  => '/etc/sssd/sssd.conf',
     order   => 05,
     content => template('sssd/domain.erb'),
