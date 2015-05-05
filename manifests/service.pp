5a5d80f38228b5cbd0d8c987ec1bfb78127f741c
Adam S <asa188@sfu.ca>
2015-05-05 08:59:43 -0700

    Added ability for it to run systemctl daemon-reload on Centos7 one time only

--- manifests/service.pp
+++ manifests/service.pp
@@ -6,6 +6,11 @@ class sssd::service {
     subscribe => File['/etc/sssd/sssd.conf'],
   }
   if ( $::osfamily == 'redhat' ) and ( $::operatingsystemversion >= 7 ) { 
+    exec { 'systemctlreload':
+      command   => 'systemctl daemon-reload',
+      onlyif    => 'test -f /etc/systemd/system/purge_sssd.service',
+      subscribe => '/etc/systemd/system/purge_sssd.service',
+    }
     service { 'purge_sssd':
       enable    => true,
     }
