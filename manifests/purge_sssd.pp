d65956e83766139e4a1f24bc01906fd5d08bbed5
Adam S <asa188@sfu.ca>
2015-05-05 09:24:44 -0700

    Fixed chmod to executable for sssd service files

--- manifests/purge_sssd.pp
+++ manifests/purge_sssd.pp
@@ -2,6 +2,6 @@ define sssd::purge_sssd {
   file { "$name":
     ensure  => file,
     content => template(join([ 'sssd/', regsubst($name,'^(.*[\\\/])', '','G'), '.erb' ], '')),
-    mode    => 0644,
+    mode    => 0700,
   }
 }
