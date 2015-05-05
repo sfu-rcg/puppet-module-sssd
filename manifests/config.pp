74a1f29d5107092389b9c1c1251fd630da6e3c1f
Adam S <asa188@sfu.ca>
2015-05-05 08:19:58 -0700

    Support for purge_sssd settings deployment, adjusted to use  inside file

--- manifests/config.pp
+++ manifests/config.pp
@@ -72,7 +72,7 @@ class sssd::config (
       # Installs required file for purge_sssd depending on OS version
       file { '$::sssd::params::purge_sssd_file':
         ensure  => file,
-        content => template(join([ 'sssd/', regsubst($::sssd::params::purge_sssd_file,'^(.*[\\\/])', '','G'), '.erb' ], '')),
+        content => template(join([ 'sssd/', regsubst($name,'^(.*[\\\/])', '','G'), '.erb' ], '')),
         #content => template(sprintf('sssd/',regsubst($::sssd::params::purge_sssd_file,'^(.*[\\\/])', '\1','G'),'.erb')),
         mode    => 0644,
       }
