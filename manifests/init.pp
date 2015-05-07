45c9410f3ddbb0767ec99f71fae49a5ce517df64
Adam S <asa188@sfu.ca>
2015-05-06 17:22:09 -0700

    test failing commit

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,7 @@ class sssd (
   validate_array( $domains, $services, ) 
 
   anchor { 'sssd::begin': }
-  anchor { 'sssd::end': }
+  anchor { 'sssd::end' }
 
 # formerly puppet::end -- don't know why
 Anchor[ 'sssd::begin' ] -> class { 'sssd::install': } -> Class['sssd::config'] ~> class { 'sssd::service': } -> class { 'sssd::purge_sssd_service': } -> Anchor[ 'sssd::end' ]
