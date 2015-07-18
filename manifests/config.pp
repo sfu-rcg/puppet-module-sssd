6f10b882ec705a80ad9353ccaf7bd3f62da23317
Adam S <asa188@sfu.ca>
2015-07-18 09:05:21 -0700

    Testing out ubuntu support for sssd in 15.04

--- manifests/config.pp
+++ manifests/config.pp
@@ -19,7 +19,7 @@ class sssd::config (
 
   concat::fragment { 'sssd.conf.base':
     target  => '/etc/sssd/sssd.conf',
-    order   => 02,
+    order   => '02',
     content => template('sssd/sssd.conf.erb'),
   }
 
@@ -28,14 +28,31 @@ class sssd::config (
       # SSSD automount retrieval in autofs 5.0.7 is busted in Ubuntu
       # see https://bugs.launchpad.net/linuxmint/+bug/1081489 for a fix
       file { '/etc/auth-client-config/profile.d/sss':
-	ensure  => file,
-	content => template('sssd/sss.erb'),
-	notify  => Exec['update-sssd-profile'],
+	      ensure  => file,
+	      content => template('sssd/sss.erb'),
+	      notify  => Exec['update-sssd-profile'],
       }
       exec { 'update-sssd-profile':
-	command     => '/usr/sbin/auth-client-config -a -p sss',
-	require     => File[ '/etc/auth-client-config/profile.d/sss' ],
-	refreshonly => true,
+        command     => '/usr/sbin/auth-client-config -a -p sss',
+       	require     => File[ '/etc/auth-client-config/profile.d/sss' ],
+       	refreshonly => true,
+      }
+      include autofs
+      if member($services, 'autofs') {
+        augeas { 'nsswitch.conf':
+          context => '/files/etc/nsswitch.conf',
+          changes => ["set /files/etc/nsswitch.conf/*[self::database = 'automount']/service[1] sss",
+                      "set /files/etc/nsswitch.conf/*[self::database = 'automount']/service[2] files",],
+          notify  => [ Service[autofs], ],
+        }
+      }
+      else {
+        augeas { 'nsswitch.conf':
+          context => '/files/etc/nsswitch.conf',
+          changes => ["set /files/etc/nsswitch.conf/*[self::database = 'automount']/service[1] files",
+                      "rm /files/etc/nsswitch.conf/*[self::database = 'automount']/service[2]",],
+          notify  => [ Service[autofs], ],
+        }
       }
     }
 
@@ -57,7 +74,7 @@ class sssd::config (
           context => '/files/etc/nsswitch.conf',
           changes => ["set /files/etc/nsswitch.conf/*[self::database = 'automount']/service[1] sss",
                       "set /files/etc/nsswitch.conf/*[self::database = 'automount']/service[2] files",],
-          notify => [ Service[autofs], ],
+          notify  => [ Service[autofs], ],
         }
       }
       else {
@@ -65,7 +82,7 @@ class sssd::config (
           context => '/files/etc/nsswitch.conf',
           changes => ["set /files/etc/nsswitch.conf/*[self::database = 'automount']/service[1] files",
                       "rm /files/etc/nsswitch.conf/*[self::database = 'automount']/service[2]",],
-          notify => [ Service[autofs], ],
+          notify  => [ Service[autofs], ],
         }
       }
     }
