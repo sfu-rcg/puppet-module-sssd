d380f28a876825b3c70f50e08562dd8239856d6d
asasfu <asa188@sfu.ca>
2015-07-18 16:39:49 -0700

    Temporary, workaround for foreman bug in 1.6.2
    Temporary, workaround for foreman bug in 1.6.2 host.shortname returns fqdn, we need the shortname
    
    We should be submitting this value via the foreman or something, this allows us to place it in in a sane default if not provided.

--- manifests/domain.pp
+++ manifests/domain.pp
@@ -1,37 +1,42 @@
 define sssd::domain (
-
-  $debug_level              = undef,
-  $dyndns_update              = false,
-  $autofs_debug_level       = undef,
-  $krb_use_fast             = undef,
-  $ldap_sasl_authid         = undef,
-  $krb5_renewable_lifetime  = undef,
-  $krb5_renew_interval 	      = undef,
-  $entry_cache_autofs_timeout = undef,
-  $autofs_provider            = undef,
-  $ldap_autofs_search_base    = undef,
+  $debug_level                    = undef,
+  $dyndns_update                  = false,
+  $autofs_debug_level             = undef,
+  $krb_use_fast                   = undef,
+  $ldap_sasl_authid               = undef, 
+  $krb5_renewable_lifetime        = undef,
+  $krb5_renew_interval 	          = undef,
+  $entry_cache_autofs_timeout     = undef,
+  $autofs_provider                = undef,
+  $ldap_autofs_search_base        = undef,
   $ldap_autofs_map_object_class   = undef,
   $ldap_autofs_entry_object_class = undef,
-  $ldap_autofs_map_name       = undef,
-  $ldap_autofs_entry_key      = undef,
-  $ldap_autofs_entry_value    = undef,
-  $id_provider                = undef, # required
-  $auth_provider              = undef, # required
-  $cache_credentials          = undef,
-  $access_provider            = undef,
-  $ldap_access_filter         = undef,
-  $ldap_id_mapping            = undef, # required
-  $min_id                     = undef,
-  $max_id                     = undef,
-  $use_fqdn                   = undef,
-  $ldap_use_tokengroups       = undef, # for murmurhash3, not POSIX GIDs
-  $ldap_id_use_start_tls      = undef,
-  $ldap_sasl_mech             = undef,
-  $ldap_access_order          = undef,
-  $ldap_account_expire_policy = undef,
-  $ldap_schema                = undef,
+  $ldap_autofs_map_name           = undef,
+  $ldap_autofs_entry_key          = undef,
+  $ldap_autofs_entry_value        = undef,
+  $id_provider                    = undef, # required
+  $auth_provider                  = undef, # required
+  $cache_credentials              = undef,
+  $access_provider                = undef,
+  $ldap_access_filter             = undef,
+  $ldap_id_mapping                = undef, # required
+  $min_id                         = undef,
+  $max_id                         = undef,
+  $use_fqdn                       = undef,
+  $ldap_use_tokengroups           = undef, # for murmurhash3, not POSIX GIDs
+  $ldap_id_use_start_tls          = undef,
+  $ldap_sasl_mech                 = undef,
+  $ldap_access_order              = undef,
+  $ldap_account_expire_policy     = undef,
+  $ldap_schema                    = undef,
   ) {
 
+  # Workaround for foreman bug in 1.6.2 host.shortname returns fqdn, we need the shortname
+  # Therefore we override this variable if it is not set via hiera, manifest or foreman(when fixed)
+  if ! $ldap_sasl_authid {
+    $ldap_sasl_authid               = upcase($::hostname) + "\$@${name}"
+  }
+  
   #notify{"SASL binding to AD as $ldap_sasl_authid":;}
 
   case $::operatingsystem {
